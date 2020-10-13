f0a9c747fecb3966ccd193f880c5ccb8ff81bab5
Darius Huntly <darius@diginclusion.com>
2020-10-13 12:34:32 +0100

    password inc caps

--- src/cyfrinair.js
+++ src/cyfrinair.js
@@ -2,19 +2,20 @@ const geiriadur = require('../assets/geiriadur.json');
 const generadu = (wordLength = 3, includeCapitals = true, includeNumbers = true) => {
   let res;
   const geiriau = [];
+  let capsDone = false;
   for(let i = 0; i<wordLength; i++) {
     let gair;
     do {
       gair = geiriadur[Math.floor(Math.random() * geiriadur.length)];
     } while(geiriau.indexOf(gair)>=0);
+
+    if(Math.random() >= 0.5&&!capsDone) {
+      capsDone = true;
+      gair = gair.toUpperCase();
+    }
+
     geiriau.push(gair);
   }
-
-  // let capsDone = false;
-
-  // for(let gair of geiriau)  {
-
-  // }
 
   if(includeNumbers) {
     geiriau.push(Math.floor(Math.random() * 100) + 1 );
